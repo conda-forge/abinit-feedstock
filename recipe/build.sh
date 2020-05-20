707e4e3955753b01a7619b2df5f019c9660d9636
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-20 20:08:05 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,17 +14,25 @@ NC_LIBS="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5"
 XC_INCS="-I${PREFIX}/include"
 XC_LIBS="-L${PREFIX}/lib -lxcf90 -lxc"
 
-CC=mpicc
-FC=mpif90
-
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
-            --with-linalg-flavor=${LINALG_FLAVOR} --with-linalg-libs="${LINALG_LIBS}" \
             --with-trio-flavor=netcdf \
-            --with-netcdf-incs="${NC_INCS}" --with-netcdf-libs="${NC_LIBS}" \
             --with-dft-flavor="wannier90-fallback" \
             --enable-gw-dpc="yes" \
+            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
+            SCALAPACK_LIBS="-L${PREFIX}/lib -lscalapack" \
+            LAPACK_LIBS="-L${PREFIX}/lib -llapack" \
+            BLAS_LIBS="-L${PREFIX}/lib -lblas" \
+            FFT_LIBS="-L${PREFIX}/lib -lfftw3" \
+            CC="mpicc" \
+            FC="mpif90" \
+            LD="mpif90 -fopenmp" \
+            CFLAGS="${CFLAGS}" \
+            FFLAGS="${FFLAGS}" \
+            CPPFLAGS="${CPPFLAGS}" \
+            --with-linalg-flavor=${LINALG_FLAVOR} --with-linalg-libs="${LINALG_LIBS}" \
+            --with-netcdf-incs="${NC_INCS}" --with-netcdf-libs="${NC_LIBS}" \
             --with-libxc-incs="${XC_INCS}" --with-libxc-libs="${XC_LIBS}"
 make -j${CPU_COUNT}
 make check
