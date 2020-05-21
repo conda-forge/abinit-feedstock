525c467444178015ec7182bff12b70858369bc14
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 06:59:00 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# FFTW3
-FFT_FLAVOR="none"
-
-# Open BLAS
-LINALG_FLAVOR="custom"
-LINALG_LIBS="-L${PREFIX}/lib -lscalapack -llapack -lblas -lpthread"
-
-NC_INCS="-I${PREFIX}/include"
-NC_LIBS="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5"
-
-# LibXC library 
-XC_INCS="-I${PREFIX}/include"
-XC_LIBS="-L${PREFIX}/lib -lxcf90 -lxc"
-
-CC=mpicc
-FC=mpif90
-
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
-            --with-linalg-flavor=${LINALG_FLAVOR} --with-linalg-libs="${LINALG_LIBS}" \
-            --with-trio-flavor=netcdf \
-            --with-netcdf-incs="${NC_INCS}" --with-netcdf-libs="${NC_LIBS}" \
-            --with-dft-flavor="wannier90-fallback" \
-            --enable-gw-dpc="yes" \
-            --with-libxc-incs="${XC_INCS}" --with-libxc-libs="${XC_LIBS}"
+            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${SRC_DIR}/S3DE/iotk/include/" \
+            CC="mpicc" \
+            FC="mpif90" \
+            CPP="${CPP}" \
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3" 
 make -j${CPU_COUNT}
 make check
-make install-exec
+make install-exe
