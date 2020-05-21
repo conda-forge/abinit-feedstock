6e96ea6a0740ec598e668859d4e442037631236b
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 09:32:07 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
             --with-trio-flavor=netcdf \
+            --with-dft-flavor=libxc \
+            --with_fft_flavor=fftw3 \
+            --with-fft-libs="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" --with-fft-incs="-I${PREFIX}/include" \
+            --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxcf90 -lxc" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
             FC="mpif90" \
