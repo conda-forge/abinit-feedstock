e40525c4a806d5f3b7328c1d356bedf5b1ebde24
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 14:39:34 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ export OMPI_MCA_rmaps_base_oversubscribe=yes
 
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
-            --enable-mpi="yes" --enable-mpi-io="yes" \
+            --enable-mpi="yes" --with-mpi-level=2 --enable-mpi-io="yes" \
             --with-trio-flavor=netcdf \
             --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
