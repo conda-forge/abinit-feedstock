7b636ce732a8b3e8fa16ed39f46c75c0f640a97d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 15:25:36 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,15 +6,14 @@
             --with-dft-flavor="libxc" \
             --with-trio-flavor=netcdf \
             --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
-            --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxcf90 -lxc" \
             --enable-gw-dpc="yes" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas" 
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" 
 make -j${CPU_COUNT}
 make check
 make install-exec
