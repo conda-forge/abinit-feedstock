bc52b45a0f942c102b59efc3ec7f0415e23735fa
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 14:23:05 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_MCA_rmaps_base_oversubscribe=yes
 
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
