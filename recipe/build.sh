79ca907567519d0c85a51e74f251aa68174941b6
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 11:21:18 +0200

    Back to basics (#8)
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.3, and conda-forge-pinning 2020.05.20.21.21.18
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.3, and conda-forge-pinning 2020.05.20.21.21.18
    
    * Update meta.yaml
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
-# FFTW3
-FFT_FLAVOR="none"
-
-# Open BLAS
-LINALG_FLAVOR="custom"
-LINALG_LIBS="-L${PREFIX}/lib -lscalapack -llapack -lblas -lpthread"
-
-NC_INCS="-I${PREFIX}/include"
-NC_LIBS="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5"
-
-# LibXC library 
-XC_INCS="-I${PREFIX}/include"
-XC_LIBS="-L${PREFIX}/lib -lxcf90 -lxc"
-
-CC=mpicc
-FC=mpif90
-
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
-            --with-linalg-flavor=${LINALG_FLAVOR} --with-linalg-libs="${LINALG_LIBS}" \
             --with-trio-flavor=netcdf \
-            --with-netcdf-incs="${NC_INCS}" --with-netcdf-libs="${NC_LIBS}" \
-            --with-dft-flavor="wannier90-fallback" \
-            --enable-gw-dpc="yes" \
-            --with-libxc-incs="${XC_INCS}" --with-libxc-libs="${XC_LIBS}"
+            --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
+            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
+            CC="mpicc" \
+            FC="mpif90" \
+            CPP="${CPP}" \
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas" 
 make -j${CPU_COUNT}
 make check
 make install-exec
