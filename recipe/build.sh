fd5778b5fb478c337e8b5aa9f1e926690c74d4c5
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 14:37:01 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
-            --with-trio-flavor=netcdf \
+            --enable-netcdf="yes" \
             --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
