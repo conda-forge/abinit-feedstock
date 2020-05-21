cf2be7e9469090c6c49a4940d0a744cedff34576
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 15:10:24 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,13 @@
 ./config/scripts/makemake
 ./configure --prefix=${PREFIX} \
             --with-mpi="yes" --enable-mpi-io="yes" \
-            --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas" 
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" 
 make -j${CPU_COUNT}
 make check
 make install-exec
