8bc643ac6385176f2b93243cd674bdcc65b9f42d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-21 10:18:10 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./config/scripts/makemake
-./configure --help
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
             --with-trio-flavor=netcdf \
@@ -9,9 +8,9 @@
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3 -lxcf90 -lxc" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3 -lxcf90 -lxc" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lscalapack -lblas -lfftw3 -lxcf90 -lxc" 
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas" 
 make -j${CPU_COUNT}
 make check
 make install-exec
