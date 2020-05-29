156e072beae6affd190e06879f99ecd99c0c102d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-29 12:52:45 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,16 @@
 ./configure --prefix=${PREFIX} \
             --enable-mpi="yes" --enable-mpi-io="yes" \
             --with-trio-flavor=netcdf \
+            --with-dft-flavor="libxc" \
+            --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxcf90 -lxc" \
             --with-netcdf-incs="-I${PREFIX}/include" --with-netcdf-libs="-L${PREFIX}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas" 
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" 
 make -j${CPU_COUNT}
 make check
 make install-exec
