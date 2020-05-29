0a70d7510ad9be079da02d7cdd3e17bfe8dd0f62
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-29 18:10:45 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,12 +7,11 @@
             --with-hdf5="yes" \
             --with-netcdf="yes" \
             --with-netcdf_fortran="yes" \
-            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
             CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc" \
+            FCFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc" \
             CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc" 
 make -j${CPU_COUNT}
 make check
