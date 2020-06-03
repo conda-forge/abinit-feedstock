63eedbf5fb46e6221ac9746508bc8c502f540ae8
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-03 10:34:45 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,14 @@
             --with-mpi="yes" --enable-mpi-io="yes" \
             --with-libxc="yes" \
             --with-hdf5="yes" \
-            --with-fft-flavor=fftw3 --with-fft-libs="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" --with-fft-incs="-I${PREFIX}/include" \
+            --with-fft-flavor=fftw3 \
             --with-netcdf="yes" \
             --with-netcdf_fortran="yes" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
+            FFT_CPPFLAGS="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" \
+            FFT_FCFLAGS="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" \
             CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3" \
             FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3f" \
             CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3" 
