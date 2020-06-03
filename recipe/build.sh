7c471a998c932209b3cdeaf85b7325465a41b193
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-03 14:40:03 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,9 +12,6 @@
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            FFTW3_CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp" \
-            FFTW3_FCFLAGS="${FFLAGS} -L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp" \
-            FFTW3_LIBS="-L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp" \
             CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
             FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
             CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
