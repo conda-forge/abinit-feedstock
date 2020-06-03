7813f6a9f7c5b892d4bc317c7a88d004f7a0ba8e
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-03 11:09:13 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@
             FC="mpif90" \
             CPP="${CPP}" \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
-            FFT_LIBS="-L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi" \
+            FFT_LIBS="-L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp" \
             CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" \
             FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" \
             CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" 
