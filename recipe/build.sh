febdfad7348bd9edc8174d087058af55d1739770
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-03 10:42:26 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@
             --with-mpi="yes" --enable-mpi-io="yes" \
             --with-libxc="yes" \
             --with-hdf5="yes" \
-            --with-fft-flavor=fftw3 \
+            --with-fft-flavor="pfft" \
             --with-netcdf="yes" \
             --with-netcdf_fortran="yes" \
             CC="mpicc" \
