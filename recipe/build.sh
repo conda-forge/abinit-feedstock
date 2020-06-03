7b8ea6debc85ab9072506c1f8ec7d6ff7e7d2d9f
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-03 10:54:38 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,17 +5,15 @@
             --with-mpi="yes" --enable-mpi-io="yes" \
             --with-libxc="yes" \
             --with-hdf5="yes" \
-            --with-fft-flavor=pfft \
             --with-netcdf="yes" \
             --with-netcdf_fortran="yes" \
             CC="mpicc" \
             FC="mpif90" \
             CPP="${CPP}" \
-            FFT_CPPFLAGS="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" \
-            FFT_FCFLAGS="-L${PREFIX}/lib/ -lfftw3 -lfftw3f" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3f" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3" 
+            FFT_LIBS="-L${PREFIX}/lib -lfftw3 -lfftw3f -lfftw3_mpi" \
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3_mpi" 
 make -j${CPU_COUNT}
 make check
 make install-exec
