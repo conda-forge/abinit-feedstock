dd800b35851c0c3e0ceccc1e18a05dcf1dd5a5ce
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-05 15:58:34 +0200

    CPP Debug (#26)
    * Update build.sh
    
    * Update meta.yaml
    
    * Update conda_build_config.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Add files via upload
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,12 +10,13 @@
             --with-netcdf_fortran="yes" \
             --with-fft-flavor="fftw3" \
             CC="mpicc" \
-            FC="mpif90" \
-            CPP="${CPP}" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp" \
-            LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -llapack -lblas -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lxcf90 -lxc -lfftw3 -lfftw3f -lfftw3f_mpi -lfftw3_mpi -lfftw3_threads -lfftw3_omp -lfftw3f_threads -lfftw3f_omp -lmpi -fopenmp"
+            FC="mpifort" \
+            CXX="mpicxx" \
+            CFLAGS="${CFLAGS}" \
+            FFLAGS="${FFLAGS}" \
+            FCFLAGS="${FCFLAGS}" \
+            CPPFLAGS="${CPPFLAGS}" \
+            LDFLAGS="${LDFLAGS} -lfftw3f"
 make -j${CPU_COUNT}
 make check
 make install-exec
